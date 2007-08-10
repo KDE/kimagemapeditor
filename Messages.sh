2c4a8c14e01957603aeb6517fc56b8c4aef67d67
Laurent Montel <montel@kde.org>
2007-08-10 20:26:30 +0000

    Fix extract message
    svn path=/trunk/KDE/kdewebdev/kimagemapeditor/; revision=698722

--- Messages.sh
+++ Messages.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
-LIST=`find . -name \*.h -o -name \*.hh -o -name \*.H -o -name \*.hxx -o -name \*.hpp -o -name \*.cpp -o -name \*.cc -o -name \*.cxx -o -name \*.ecpp -o -name \*.C`
+LIST=`find . -name \*.h -o -name \*.cpp`
 if test -n "$LIST"; then 
 	$XGETTEXT $LIST -o $podir/kimagemapeditor.pot 
 fi
